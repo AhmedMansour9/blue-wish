7fa999d254ad123fd005da9dba472856a586a01a
ahmed <ahmedmansour2194@gmail.com>
2026-09-24 14:32:46 +0300

    changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,7 +13,7 @@ import FBSDKLoginKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    GMSServices.provideAPIKey("YOUR_MAP_KEY_HERE")
+    GMSServices.provideAPIKey("AIzaSyAYG5r1YDTzXL-_HTLaBUmngS15zUs4zdU")
     FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -190,7 +190,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                       padding: const EdgeInsets.all(Dimensions.paddingSizeDefault),
                                       child: Directionality(
                                         textDirection: TextDirection.ltr,
-                                        child: Image.asset(Images.logoWithNameImage, width: 140, height: 50)
+                                        child: Image.asset(Images.logo, width: 120, height: 120, fit: BoxFit.contain)
                                       ),
                                     ),
                                   ),
